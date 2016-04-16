8771811689c0354d5f4da7bf98e1cd88a4924b2b
admin <admin@admins-Air.home>
2016-04-16 11:54:25 -0400

    Solutions for some Chapter 3 exercises

---
+++ ch03/Exercises.hs
@@ -0,0 +1,42 @@
+
+----
+-- 1. Write the converse of fromList for the List type: a function that takes a 
+--    List a and generates a [a]
+data List a = Cons a (List a)
+            | Nil
+            deriving (Show)
+
+fromList::List a -> [a]
+fromList Nil = []
+fromList (Cons x xs) = [x] ++ fromList(xs)
+-- testing 
+-- []      == fromList Nil
+-- [1]     == fromList (Cons 1 Nil)
+-- [1,2]   == fromList (Cons 1 (Cons 2 Nil))
+
+
+----
+-- 2. Define a tree type that has only one constructor, like our Java example. 
+-- Instead of the Empty constructor, use the Maybe type to refer to a node's 
+-- children. Below is previous defintion
+-- data Tree a = Node a (Tree a) (Tree a)
+--             | Empty
+--             deriving (Show)
+data MyTree a = Node a (Maybe (MyTree a)) (Maybe (MyTree a))
+              deriving (Show)
+-- testing
+-- let l = Node 1 Nothing Nothing
+-- let r = Node 5 Nothing Nothing
+-- let n = Node 3 (Just l) (Just r)
+
+
+----
+-- 1. Write a function that computes the number of elements in a list. To test 
+-- it, ensure that it gives the same answers as the standard length function.
+-- 2. Add a type signature for your function to your source file. To test it, 
+-- load the source file into ghci again.
+-- length_of_list::Num a => [t] -> a --Alternate type signature
+length_of_list::[t] -> Int
+length_of_list[] = 0
+length_of_list(x:xs) = 1 + length_of_list(xs)
+
