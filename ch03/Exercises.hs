22d0dfeb543767eef6a4fb507c1a2360d397afff
admin <admin@admins-MacBook-Air.local>
2016-04-26 22:31:46 -0400

    Completed upto exercise 9

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -27,9 +27,9 @@ fromList (Cons x xs) = [x] ++ fromList(xs)
 data MyTree a = Node a (Maybe (MyTree a)) (Maybe (MyTree a))
               deriving (Show)
 -- testing
--- let l = Node 1 Nothing Nothing
--- let r = Node 5 Nothing Nothing
--- let n = Node 3 (Just l) (Just r)
+-- l = Node 1 Nothing Nothing
+-- r = Node 5 Nothing Nothing
+-- n = Node 3 (Just l) (Just r)
 
 
 ----
@@ -99,3 +99,47 @@ myintersperse a (xs:ys:zs) = xs ++ [a] ++ ys ++ [a] ++ (myintersperse a zs)
 -- "foo,bar,baz"
 -- myintersperse  ',' ["foo", "bar", "baz", "quz"]
 -- "foo,bar,baz,quz"
+
+
+----
+-- 8. Using the binary tree type that we defined earlier in this chapter, write
+-- a function that will determine the height of the tree. The height is the l
+-- argest number of hops from the root to an Empty. 
+-- For example, the tree Empty has height zero; 
+--    Node "x" Empty Empty has height one; 
+--    Node "x" Empty (Node "y" Empty Empty) has height two; and so on.
+data Tree a = Node2 a (Tree a) (Tree a)
+            | Empty
+            deriving (Show)
+tree_height::(Num b, Ord b)=>Tree a -> b
+tree_height Empty = 0
+tree_height (Node2 x (left) (right)) = 1 + (max (tree_height left) (tree_height right))
+
+-- left2 = Node2 10 Empty Empty
+-- left1 = Node2 50 left2 Empty
+-- right1 = Node2 150 Empty Empty
+-- root = Node2 100 left1 right1
+-- tree_height root == 3
+-- tree_height left2 == 1
+-- tree_height Empty == 0
+
+----
+-- 9. Consider three two-dimensional points a, b, and c. If we look at the 
+-- angle formed by the line segment from a to b and the line segment from b to
+-- c, it either turns left, turns right, or forms a straight line. Define a 
+-- Direction data type that lets you represent these possibilities.
+data Point2D = Point2D Int Int
+                deriving (Show)
+data Segment = Segment Point2D Point2D
+                deriving (Show)
+data Direction = Left Segment Segment
+                | Right Segment Segment
+                | Line Segment Segment
+                deriving (Show)
+                
+-- a = Point2D 0 0
+-- b = Point2D 0 2
+-- c = Point2D 3 2
+-- ab = Segment a b
+-- bc = Segment b c
+-- my_abc = Main.Right ab bc
